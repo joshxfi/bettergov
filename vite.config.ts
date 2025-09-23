42ef6509c014e6a8c220f27fd7a06feab6b3b15c
Nicu Listana <niculistana@gmail.com>
2025-09-22 19:33:57 -0700

    chore(config): use @kapwa-design-system alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@kapwa-design-system': '/src/components/kapwa-design-system',
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
